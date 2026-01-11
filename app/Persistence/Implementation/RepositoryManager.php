555625d45b6cce362851c029d128fa368b2f2565
Pedro Duarte <pedro.duarte@neos-tecnologia.com.br>
2026-01-11 15:06:45 -0300

    chore: Adiciona métodos de transação para gerenciador

--- app/Persistence/Implementation/RepositoryManager.php
+++ app/Persistence/Implementation/RepositoryManager.php
@@ -4,15 +4,34 @@ namespace App\Persistence\Implementation;
 
 use App\Persistence\Implementation\Repositories\UsersRepository;
 use App\Persistence\Interfaces\RepositoryManagerInterface;
+use Illuminate\Support\Facades\DB;
 
 class RepositoryManager implements RepositoryManagerInterface
 {
+    private $dbConnection;
+
     public function __construct()
-    {}
+    {
+        $this->dbConnection = DB::connection();
+    }
 
     public function getUsersRepository(): UsersRepository
     {
         return new UsersRepository();
     }
 
+    public function beginTransaction(): void
+    {
+        $this->dbConnection->beginTransaction();
+    }
+
+    public function commitTransaction(): void
+    {
+        $this->dbConnection->commit();
+    }
+
+    public function rollbackTransaction(): void
+    {
+        $this->dbConnection->rollBack();
+    }
 }
--- app/Persistence/Interfaces/RepositoryManagerInterface.php
+++ app/Persistence/Interfaces/RepositoryManagerInterface.php
@@ -7,4 +7,7 @@ use App\Persistence\Implementation\Repositories\UsersRepository;
 interface RepositoryManagerInterface
 {
     public function getUsersRepository(): UsersRepository;
+    public function beginTransaction(): void;
+    public function commitTransaction(): void;
+    public function rollbackTransaction(): void;
 }
