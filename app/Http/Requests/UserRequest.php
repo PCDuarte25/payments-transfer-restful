367afdd3d0a8a492be32efd8209403a28a873710
Pedro Duarte <pedro.duarte@neos-tecnologia.com.br>
2026-01-11 15:42:40 -0300

    refactor: Remove validação por unique de email e documento do request

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -25,11 +25,10 @@ class UserRequest extends FormRequest
     public function rules() {
         return [
             'full_name' => ['required', 'string', 'max:255'],
-            'email'     => ['required', 'email', 'unique:users,email'],
+            'email'     => ['required', 'email'],
             'document'  => [
                 'required',
                 'string',
-                'unique:users,document',
                 'regex:/^\d{11}$|^\d{3}\.\d{3}\.\d{3}-\d{2}$/'
             ],
             'password'  => ['required', 'string', 'min:6'],
@@ -40,8 +39,6 @@ class UserRequest extends FormRequest
     public function messages()
     {
         return [
-            'email.unique' => 'Este endereço de e-mail já existe.',
-            'document.unique' => 'Este CPF/CNPJ já está cadastrado.',
             'document.regex' => 'O formato do documento é inválido.',
             'user_type.in' => 'O tipo de usuário deve ser "common" (comum) ou "merchant" (lojista).',
         ];
